166e19255d7fde7bf6f7d58f69eee423476edae1
hdtopku <1450948930@qq.com>
2024-04-26 21:02:25 +0100

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-red-500">App
+    <p>This is the client-side React app.</p>
+    </h1>
+  )
+}
