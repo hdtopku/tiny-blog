475b50bce61a93950d90623315934b819a8205fd
hdtopku <1450948930@qq.com>
2024-04-28 13:54:16 +0100

    create sign api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,41 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import {errorHandler} from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signUp = async (req, res,next) => {
-  const { username, email, password } = req.body;
-  if(!username ||!email ||!password) {
+export const signUp = async (req, res, next) => {
+  const {username, email, password} = req.body;
+  if (!username || !email || !password) {
     next(errorHandler(400, "Please provide all required fields"))
   }
   try {
-    await User.create({ username, email, password: bcryptjs.hashSync( password,10) });
-    res.status(200).json({ success: true, message: "Sing up successfully" });
+    await User.create({username, email, password: bcryptjs.hashSync(password, 10)});
+    res.status(200).json({success: true, message: "Sing up successfully"});
   } catch (error) {
     next(error);
   }
 };
+
+export const signIn = async (req, res, next) => {
+  const {username, password} = req.body;
+  if (!username || !password) {
+    next(errorHandler(400, "Please provide all required fields"))
+  }
+  try {
+    const user = await User.findOne({username});
+    if (!user) {
+      return next(errorHandler(400, "Username or password is incorrect"));
+    }
+    const isMatch = bcryptjs.compareSync(password, user.password);
+    if (!isMatch) {
+      return next(errorHandler(400, "Username or password is incorrect"));
+    }
+    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY);
+    const {password: pass, ...withoutPass} = user._doc
+    return res.status(200).cookie('access_token', token, {
+      httpOnly: true
+    }).json({success: true, message: 'Sign in successfully', data: withoutPass});
+  } catch (error) {
+    return next(error);
+  }
+}
--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGO)
   .then(() => console.log('Connected to database')).catch(err => console.log(err));
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // To parse JSON requests
 app.listen(3000, () => {
   console.log('Server is running on port 3000!!!');
 });
